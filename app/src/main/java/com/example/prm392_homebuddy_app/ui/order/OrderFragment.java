ae3c36aab26dd86bfea4fb566259f479846bf322
1903giabao <baogavip00@gmail.com>
2024-10-26 22:20:29 +0700

    billingScreen

--- app/src/main/java/com/example/prm392_homebuddy_app/ui/order/OrderFragment.java
+++ app/src/main/java/com/example/prm392_homebuddy_app/ui/order/OrderFragment.java
@@ -2,6 +2,7 @@ package com.example.prm392_homebuddy_app.ui.order;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +12,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.prm392_homebuddy_app.R;
 
-public class OrderFragment extends Fragment {
+import java.util.Calendar;
 
+public class OrderFragment extends Fragment {
+    private EditText edtDate;
     private OrderViewModel mViewModel;
 
     public static OrderFragment newInstance() {
@@ -26,6 +31,30 @@ public class OrderFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        edtDate = view.findViewById(R.id.edtServiceDate);
+
+        edtDate.setOnClickListener(v -> {
+            // Get the current date
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a DatePickerDialog
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (DatePicker view1, int selectedYear, int selectedMonth, int selectedDay) -> {
+                        // Set the selected date in the EditText
+                        edtDate.setText(selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear);
+                    }, year, month, day);
+
+            datePickerDialog.show();
+        });
     }
 
     @Override
