e52195c4bc02ddae53c9ce430f4c949ac7c615e4
thangbinhbeo <binhbeopro1122@gmail.com>
2024-10-29 12:43:33 +0700

    oke man

---
+++ app/src/main/java/com/example/prm392_homebuddy_app/CartAcitivity.java
@@ -0,0 +1,84 @@
+package com.example.prm392_homebuddy_app;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.prm392_homebuddy_app.API.CartAPI;
+import com.example.prm392_homebuddy_app.API.ServiceRepository;
+import com.example.prm392_homebuddy_app.adapters.CartAdapter;
+import com.example.prm392_homebuddy_app.model.Cart;
+import com.example.prm392_homebuddy_app.model.CartResponse;
+import com.example.prm392_homebuddy_app.ui.order.OrderFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class CartAcitivity extends AppCompatActivity {
+
+    private ListView listViewCart;
+    private TextView subtotalTextView;
+    private TextView total;
+    private CartAdapter cartAdapter;
+    private List<Cart> cartItems;
+    private CartAPI cartService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        listViewCart = findViewById(R.id.listCart);
+        subtotalTextView = findViewById(R.id.subtotal);
+        total = findViewById(R.id.total);
+
+        cartItems = new ArrayList<>();
+        cartAdapter = new CartAdapter(this, cartItems);
+        listViewCart.setAdapter(cartAdapter);
+
+        cartService = ServiceRepository.getCartAPI();
+
+        //chua co login userid
+        int userId = 1;
+        loadCartData(userId);
+    }
+
+    private void loadCartData(int userId){
+        try {
+            Call<CartResponse> call = cartService.getCartItems(userId);
+
+            call.enqueue(new Callback<CartResponse>() {
+                @Override
+                public void onResponse(Call<CartResponse> call, Response<CartResponse> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        CartResponse cartResponse = response.body();
+
+                        cartItems.clear();
+                        cartItems.addAll(cartResponse.getCartItems());
+                        cartAdapter.notifyDataSetChanged();
+
+                        subtotalTextView.setText("$" + cartResponse.getSubtotal());
+                        total.setText("$" + cartResponse.getSubtotal());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<CartResponse> call, Throwable t) {
+                    Toast.makeText(CartAcitivity.this, "Failed to load cart data", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch (Exception ex){
+            Log.d("Loi", ex.getMessage());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prm392_homebuddy_app/model/Cart.java
@@ -0,0 +1,89 @@
+package com.example.prm392_homebuddy_app.model;
+
+import java.util.Date;
+
+public class Cart {
+    public int userId;
+    public int serviceId;
+    public int quantity;
+    public double price;
+    public Date createAt;
+    private String serviceName;
+    private double servicePrice;
+    private String serviceImage;
+
+    public Cart(int userId, int serviceId, int quantity, double price, Date createAt, String serviceName, double servicePrice, String serviceImage) {
+        this.userId = userId;
+        this.serviceId = serviceId;
+        this.quantity = quantity;
+        this.price = price;
+        this.createAt = createAt;
+        this.serviceName = serviceName;
+        this.servicePrice = servicePrice;
+        this.serviceImage = serviceImage;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public double getServicePrice() {
+        return servicePrice;
+    }
+
+    public void setServicePrice(double servicePrice) {
+        this.servicePrice = servicePrice;
+    }
+
+    public String getServiceImage() {
+        return serviceImage;
+    }
+
+    public void setServiceImage(String serviceImage) {
+        this.serviceImage = serviceImage;
+    }
+}
