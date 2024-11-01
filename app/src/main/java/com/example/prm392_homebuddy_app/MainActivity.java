e42e96026a7d0ea3db0fcee92cd2eee0c3cc474b
tiivoooo <tienv5995@gmail.com>
2024-11-01 17:00:46 +0700

    booking layout

--- app/src/main/java/com/example/prm392_homebuddy_app/MainActivity.java
+++ app/src/main/java/com/example/prm392_homebuddy_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.prm392_homebuddy_app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         if (PreferenceUtils.isLoggedIn(this) && !PreferenceUtils.isTokenExpired(this)) {
             String role = PreferenceUtils.getUserRole(this);
