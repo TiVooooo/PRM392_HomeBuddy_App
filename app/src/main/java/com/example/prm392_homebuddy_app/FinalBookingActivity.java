3b4d911933945a81a1f206ed639686a334e82086
thangbinhbeo <binhbeopro1122@gmail.com>
2024-11-02 03:12:28 +0700

    view

---
+++ app/src/main/java/com/example/prm392_homebuddy_app/FinalBookingActivity.java
@@ -0,0 +1,17 @@
+package com.example.prm392_homebuddy_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FinalBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.final_bill);
+
+
+    }
+}
